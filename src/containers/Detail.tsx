75454a497a9647ccc50dbc82aca2499e31a0ee81
goduarziha <goudarziha@gmail.com>
2020-02-14 00:30:01 -0500

    small ting

--- src/containers/Detail.tsx
+++ src/containers/Detail.tsx
@@ -1,15 +1,46 @@
 import * as React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import _ from "lodash";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const Detail = () => {
   const { id } = useParams();
+  const workouts = useSelector(state => _.get(state, ["workouts", "users"]));
 
-  const workout = useSelector(state => {
-    _.get(state, ["workouts", "users"]);
-  });
-  return <div>{id}</div>;
+  let workout;
+  if (id !== undefined) {
+    workout = workouts[_.findIndex(workouts, { id: parseInt(id) })];
+  }
+  console.log(workout);
+  return (
+    <div className="container">
+      <h1>{workout.name}</h1>
+      <Link to="/">
+        <span>by: {workout.user.username}</span>
+      </Link>
+      <span>yes</span>
+      <Tabs>
+        <TabList>
+          <Tab>Overview</Tab>
+          <Tab>Reviews</Tab>
+          <Tab>Comments</Tab>
+          <Tab>Variants</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>{workout.description}</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>yes</h2>
+        </TabPanel>
+        <TabPanel>
+          <h3>yes</h3>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Detail;
